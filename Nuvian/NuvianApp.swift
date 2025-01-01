7330e7eccb773f3e089dac4bb19b25ba78c1c3d3
arjunbreddy22 <arjunreddy5275@gmail.com>
2024-12-31 16:14:25 -0800

    Initial Commit

---
+++ Nuvian/NuvianApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuvianApp.swift
+//  Nuvian
+//
+//  Created by Arjun Reddy on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct NuvianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NuvianTests/NuvianTests.swift
@@ -0,0 +1,17 @@
+//
+//  NuvianTests.swift
+//  NuvianTests
+//
+//  Created by Arjun Reddy on 12/31/24.
+//
+
+import Testing
+@testable import Nuvian
+
+struct NuvianTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
